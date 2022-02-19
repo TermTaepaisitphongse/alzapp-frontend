bc26f282ec246172b4d64e31ab8f720ed6c9881f
Term <40206647+TermTaepaisitphongse@users.noreply.github.com>
2022-02-18 22:19:22 -0500

    wip homepage

---
+++ components/Navbar.js
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+import { Menu, Dropdown, Row, Col, Space, Divider } from "antd";
+import Link from "next/link";
+import { AiOutlineLogout } from "react-icons/ai";
+
+const StyledNavbar = styled.div`
+  background: blue;
+  color: white;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  a {
+    color: white;
+  }
+`;
+
+const Navbar = () => {
+  const Services = (
+    <Menu>
+      <Menu.Item>
+        <Link href="/medicalRecords">
+          <a target="_blank" rel="noopener noreferrer">
+            Medical Records
+          </a>
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          To-Do List
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.luohanacademy.com"
+        >
+          Calendar
+        </a>
+      </Menu.Item>
+      <Menu.Item>Games</Menu.Item>
+    </Menu>
+  );
+
+  const AboutUs = (
+    <Menu>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Inspiration
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          Feedback
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <StyledNavbar>
+      <div>
+        AlzApp
+        <Divider type="vertical" />
+        <Space>
+          <Dropdown overlay={Services}>
+            <a
+              className="ant-dropdown-link"
+              onClick={(e) => e.preventDefault()}
+            >
+              Services
+            </a>
+          </Dropdown>
+          <Divider type="vertical" />
+          <Dropdown overlay={AboutUs}>
+            <a
+              className="ant-dropdown-link"
+              onClick={(e) => e.preventDefault()}
+            >
+              About Us
+            </a>
+          </Dropdown>
+        </Space>
+      </div>
+      <div>
+        <Space split={<Divider type="vertical" />}>
+          <a>Hello! Jan 24</a>
+          <Divider type="vertical" />
+          <Dropdown overlay={AboutUs}>
+            <a
+              className="ant-dropdown-link"
+              onClick={(e) => e.preventDefault()}
+            >
+              English
+            </a>
+            <Divider type="vertical" />
+            <a href="google.com" target="_blank">
+              <AiOutlineLogout />
+              Logout
+            </a>
+          </Dropdown>
+        </Space>
+      </div>
+    </StyledNavbar>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "antd/dist/antd.css";
+import "../styles/vars.css";
+import "../styles/global.css";
+import Navbar from "../components/Navbar";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
