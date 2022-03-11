5a8a5132b2559e237bd2b9385d6bfc88d17e1c12
Term <40206647+TermTaepaisitphongse@users.noreply.github.com>
2022-03-11 18:48:15 +0700

    homepage

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,25 @@
 import styled from "styled-components";
-import { Menu, Dropdown, Row, Col, Space, Divider } from "antd";
+import { Menu, Dropdown, Row, Col, Space, Divider, Grid } from "antd";
 import Link from "next/link";
 import { AiOutlineLogout } from "react-icons/ai";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
+const { useBreakpoint } = Grid;
 const StyledNavbar = styled.div`
-  background: blue;
-  color: white;
+  background: white;
+  color: #4a6fa5;
   height: 50px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 10px;
   a {
-    color: white;
+    color: #4a6fa5;
+    font-weight: bold;
   }
 `;
 
@@ -71,52 +78,106 @@ const Navbar = () => {
     </Menu>
   );
 
-  return (
-    <StyledNavbar>
+  const { SubMenu } = Menu;
+  const screens = useBreakpoint();
+  console.log("screens", screens);
+
+  if (screens.xs) {
+    return (
       <div>
-        AlzApp
-        <Divider type="vertical" />
-        <Space>
-          <Dropdown overlay={Services}>
-            <a
-              className="ant-dropdown-link"
-              onClick={(e) => e.preventDefault()}
-            >
-              Services
-            </a>
-          </Dropdown>
-          <Divider type="vertical" />
-          <Dropdown overlay={AboutUs}>
-            <a
-              className="ant-dropdown-link"
-              onClick={(e) => e.preventDefault()}
-            >
-              About Us
-            </a>
-          </Dropdown>
-        </Space>
+        <Menu
+          style={{ width: 256 }}
+          defaultSelectedKeys={["1"]}
+          defaultOpenKeys={["sub1"]}
+          mode="inline"
+        >
+          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+            <Menu.ItemGroup key="g1" title="Item 1">
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup key="g2" title="Item 2">
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            icon={<AppstoreOutlined />}
+            title="Navigation Two"
+          >
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+          </SubMenu>
+          <SubMenu
+            key="sub4"
+            icon={<SettingOutlined />}
+            title="Navigation Three"
+          >
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <Menu.Item key="11">Option 11</Menu.Item>
+            <Menu.Item key="12">Option 12</Menu.Item>
+          </SubMenu>
+        </Menu>
       </div>
-      <div>
-        <Space split={<Divider type="vertical" />}>
-          <a>Hello! Jan 24</a>
+    );
+  } else {
+    return (
+      <StyledNavbar>
+        <div>
+          AlzApp
           <Divider type="vertical" />
-          <Dropdown overlay={AboutUs}>
+          <Space>
+            <Dropdown overlay={Services}>
+              <a
+                className="ant-dropdown-link"
+                onClick={(e) => e.preventDefault()}
+              >
+                Services
+              </a>
+            </Dropdown>
+            <Divider type="vertical" />
+            <Dropdown overlay={AboutUs}>
+              <a
+                className="ant-dropdown-link"
+                onClick={(e) => e.preventDefault()}
+              >
+                About Us
+              </a>
+            </Dropdown>
+          </Space>
+        </div>
+        <div>
+          <Space split={<Divider type="vertical" />}>
+            <a>Hello! Jan 24</a>
+            <Divider type="vertical" />
+            <Dropdown overlay={AboutUs}>
+              <a
+                className="ant-dropdown-link"
+                onClick={(e) => e.preventDefault()}
+              >
+                English
+              </a>
+            </Dropdown>
+            <Divider type="vertical" />
             <a
-              className="ant-dropdown-link"
-              onClick={(e) => e.preventDefault()}
+              href="google.com"
+              target="_blank"
+              style={{ display: "flex", alignItems: "center" }}
             >
-              English
+              <AiOutlineLogout style={{ marginRight: 4 }} />
+              Logout
             </a>
-          </Dropdown>
-          <Divider type="vertical" />
-          <a href="google.com" target="_blank">
-            <AiOutlineLogout />
-            Logout
-          </a>
-        </Space>
-      </div>
-    </StyledNavbar>
-  );
+          </Space>
+        </div>
+      </StyledNavbar>
+    );
+  }
 };
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,29 @@ import styles from "../styles/Home.module.css";
 import { Button } from "antd";
 import { Menu, Dropdown, Row, Col, Space, Divider } from "antd";
 import styled from "styled-components";
+import { GoChecklist } from "react-icons/go";
+import { AiFillInfoCircle } from "react-icons/ai";
+import { RiServiceFill } from "react-icons/ri";
 
 const StyleHome = styled.div``;
 const InfoCard = styled.div`
-  background-color: gray;
+  background-color: white;
+  border: 2px solid #4a6fa5;
+  border-radius: 6px;
   height: 350px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 24px;
+    font-weight: bold;
+    color: #4a6fa5;
+  }
 `;
 const StyleImage = styled.div`
-  background-image: url(/flower.jpeg);
+  background-image: url(/homepage-image.jpg);
   background-repeat: no-repeat;
   background-size: cover;
   height: 300px;
@@ -21,15 +36,24 @@ export default function Home() {
   return (
     <>
       <StyleImage />
-      <Row gutter={[24, 24]} style={{ padding: 24 }}>
-        <Col span={8}>
-          <InfoCard>Services</InfoCard>
+      <Row gutter={[24, 24]} style={{ padding: 24, marginTop: 12 }}>
+        <Col xs={24} md={8}>
+          <InfoCard>
+            <GoChecklist size={200} style={{ color: "#4A6FA5" }} />
+            <p>Services</p>
+          </InfoCard>
         </Col>
-        <Col span={8}>
-          <InfoCard>Health Info</InfoCard>
+        <Col xs={24} md={8}>
+          <InfoCard>
+            <AiFillInfoCircle size={200} style={{ color: "#4A6FA5" }} />
+            <p>Health Info</p>
+          </InfoCard>
         </Col>
-        <Col span={8}>
-          <InfoCard>About Us</InfoCard>
+        <Col xs={24} md={8}>
+          <InfoCard>
+            <RiServiceFill size={200} style={{ color: "#4A6FA5" }} />
+            <p>About Us</p>
+          </InfoCard>
         </Col>
       </Row>
     </>
